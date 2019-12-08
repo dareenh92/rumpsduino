d95c1c7930a4cf93a427275d5a35d5ac19b3b0b2
dareenh92 <40022346+dareenh92@users.noreply.github.com>
2019-12-08 11:49:46 +0700

    Edited serial function example

--- examples/serial_test/serial_test.ino
+++ examples/serial_test/serial_test.ino
@@ -1,38 +1,49 @@
-
+//*******
 // Core 0 program
+//*******
 void setup()
 {
-  serial_begin(0x1);
+  serial_begin(0x1); //baud rate of 115200
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
+  // wait for character 'j'
+  while(serial_getchar()!='j');
+
+  // sends message
   serial_putchar('A');
-  serial_print(" HELLO ");
+  serial_print(" hello ");
   serial_println("world");
-  delay(500);
 }
 
+//*******
 // Core 1 program
+//*******
+unsigned int core1_delay;
+const int core1_led = 0;
 void setup1()
 {
-  del1 = 200000;
-  pinMode(32, OUTPUT);
+  core1_delay = 200000;
+  pinMode(core1_led, OUTPUT);
+  digitalWrite(core1_led, HIGH);
 }
 
 void loop1()
 {
-  digitalWrite(32, HIGH);
-  delayMicroseconds(del1);
-  digitalWrite(32, LOW);
-  delayMicroseconds(del1);
+  digitalWrite(core1_led, !digitalRead(core1_led));
+  delayMicroseconds(core1_delay);
 }
 
+//*******
 // Core 2 program
+//*******
 void setup2() {}
 void loop2() {}
 
+//*******
 // Core 3 program
+//*******
 void setup3() {}
 void loop3() {}
